b5e50e52dc4b7bd8400e606994028bb3861907a6
Angelo Corsaro <corsaro@doc.ece.uci.edu>
2002-12-05 04:07:21 +0000

    ChangeLogTag: Wed Dec  4 20:16:52 2002  Angelo Corsaro  <corsaro@doc.ece.uci.edu>
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTJPerf/trunk@2615 bfff1265-3dd4-0310-90a8-ce2828190deb

--- src/edu/uci/ece/doc/rtjperf/mem/timing/ScopedMemoryTimingTest.java
+++ src/edu/uci/ece/doc/rtjperf/mem/timing/ScopedMemoryTimingTest.java
@@ -1,5 +1,5 @@
 // ************************************************************************
-//    $Id: ScopedMemoryTimingTest.java,v 1.3 2002/12/01 04:25:48 corsaro Exp $
+//    $Id: ScopedMemoryTimingTest.java,v 1.4 2002/12/05 04:07:21 corsaro Exp $
 // ************************************************************************
 //
 //                               RTJPerf
@@ -48,177 +48,190 @@ import edu.uci.ece.doc.rtjperf.mem.MemoryAreaFactory;
  */
 public class ScopedMemoryTimingTest {
 
-    final static String CREATION_TIME = "CreationTime";
-    final static String ENTER_TIME = "EnterTime";
-    final static String EXIT_TIME = "ExitTime";
-    final static String EXEC_TIME = "ExecTime";
+  final static String CREATION_TIME = "CreationTime";
+  final static String ENTER_TIME = "EnterTime";
+  final static String EXIT_TIME = "ExitTime";
+  final static String EXEC_TIME = "ExecTime";
     
-    static class Item {
-        private double field0;
-        private double field1;
-        private double field2;
-        private double field3;
-        private double field4;
+  static class Item {
+    private double field0;
+    private double field1;
+    private double field2;
+    private double field3;
+    private double field4;
         
-      //      protected void finalize() {
-        // We don't do anything but, to check if Scoped memory are
-        // finilizing the object allocated we can print something
-        // from here.
-        // System.out.print("o");
-      //      }
-    }
+    //      protected void finalize() {
+    // We don't do anything but, to check if Scoped memory are
+    // finilizing the object allocated we can print something
+    // from here.
+    // System.out.print("o");
+    //      }
+  }
 
-    static class AllocatorLogic implements Runnable {
+  static class AllocatorLogic implements Runnable {
 
-        private final int SLACK = 1024;
-        private MemoryArea scopedMemory;
-        private HighResTimer timerEnter;
-        private HighResTimer timerExit;
-        private HighResTimer timerExec;
+    private final int SLACK = 1024;
+    private MemoryArea scopedMemory;
+    private HighResTimer timerEnter;
+    private HighResTimer timerExit;
+    private HighResTimer timerExec;
       
-        AllocatorLogic(HighResTimer timerEnter,
-                       HighResTimer timerExit,
-                       HighResTimer timerExec) {
-            this.timerEnter = timerEnter;
-            this.timerExit = timerExit;
-            this.timerExec = timerExec;
-        }
+    AllocatorLogic(HighResTimer timerEnter,
+                   HighResTimer timerExit,
+                   HighResTimer timerExec) {
+      this.timerEnter = timerEnter;
+      this.timerExit = timerExit;
+      this.timerExec = timerExec;
+    }
 
-        void setScopedMemory(MemoryArea scopedMemory) {
-            this.scopedMemory = scopedMemory;
-        }
+    void setScopedMemory(MemoryArea scopedMemory) {
+      this.scopedMemory = scopedMemory;
+    }
 
-        public void run() {
-            timerEnter.stop();
-            timerExec.start();
-            while (this.scopedMemory.memoryRemaining() > SLACK) {
-                new Item();
-            }
-            timerExec.stop();
-            timerExit.start();
-        }
-    }                                              
+    public void run() {
+      timerEnter.stop();
+      timerExec.start();
+      while (this.scopedMemory.memoryRemaining() > SLACK) {
+        new Item();
+      }
+      timerExec.stop();
+      timerExit.start();
+    }
+  }                                              
     
-    static class TestLogic implements Runnable {
+  static class TestLogic implements Runnable {
 
-        private int count;
-        private long memSize;
-        private int memType;
-        private String outDir;
+    private int count;
+    private long memSize;
+    private int memType;
+    private String outDir;
         
-        public TestLogic(int count, long memSize, int memType, String outDir) {
-            this.count = count;
-            this.memSize = memSize;
-            this.memType = memType;
-            this.outDir = outDir;
-        }
+    public TestLogic(int count, long memSize, int memType, String outDir) {
+      this.count = count;
+      this.memSize = memSize;
+      this.memType = memType;
+      this.outDir = outDir;
+    }
 
-        public void run() {
-            //            System.out.println("---------------------> Test Started <------------------ ");
-            HighResTimer timerEnter = new HighResTimer();
-            HighResTimer timerExit = new HighResTimer();
-            HighResTimer timer = new HighResTimer();
-            PerformanceReport report =
-                new PerformanceReport("ScopedMemoryTiming" + this.memType);
+    public void run() {
+      //            System.out.println("---------------------> Test Started <------------------ ");
+      final HighResTimer timerEnter = new HighResTimer();
+      final HighResTimer timerExit = new HighResTimer();
+      final HighResTimer timerExec = new HighResTimer();
+      final HighResTimer timer = new HighResTimer();
+      PerformanceReport report =
+        new PerformanceReport("ScopedMemoryTiming" + this.memType);
 
-            AllocatorLogic allocatorLogic = new AllocatorLogic(timerEnter, timerExit, timer);
+      final AllocatorLogic allocatorLogic = new AllocatorLogic(timerEnter, timerExit, timerExec);
 
-            timerEnter.start();
-            timerEnter.stop();
-            timerEnter.reset();
+      timerEnter.start();
+      timerEnter.stop();
+      timerEnter.reset();
             
-            timerExit.start();
-            timerExit.stop();
-            timerExit.reset();
+      timerExit.start();
+      timerExit.stop();
+      timerExit.reset();
 
+      timer.start();
+      timer.stop();
+      timer.reset();
+
+      // This memory area is used as a bootstram memory area within
+      // which is created the Big Memory area that runs the test. This
+      // guarantees that once we exit the bootstrap memory area (i.e. 
+      // at each iteration of the test the big Memory Area is
+      // automatically cleaned up)
+      final CTMemory bootstrapMA = new CTMemory(1024 * 1024);
+
+      final Runnable bootstrapLogic = new Runnable() {
+          public void run() {
             timer.start();
+            MemoryArea memoryArea =
+              MemoryAreaFactory.createMemoryArea(memSize, memSize, memType);
             timer.stop();
-            timer.reset();
+                  
+            allocatorLogic.setScopedMemory(memoryArea);
+                  
+            timerEnter.start();
+            memoryArea.enter(allocatorLogic);
+            timerExit.stop();
+                  
+          }
+        };
             
-            for (int i = 0; i < this.count; ++i) {
-                //                System.out.print(".");
-                timer.start();
-                MemoryArea memoryArea =
-                    MemoryAreaFactory.createMemoryArea(memSize, memSize, memType);
-                timer.stop();
-
-                report.addMeasuredVariable(CREATION_TIME + memSize, timer.getElapsedTime());
-                timer.reset();
-
-                allocatorLogic.setScopedMemory(memoryArea);
-                
-                //                timer.start();
-                timerEnter.start();
-                memoryArea.enter(allocatorLogic);
-                timerExit.stop();
-                //                timer.stop();
-
-                report.addMeasuredVariable(ENTER_TIME + memSize, timerEnter.getElapsedTime());
-                timerEnter.reset();
-
-                report.addMeasuredVariable(EXIT_TIME + memSize, timerExit.getElapsedTime());
-                timerExit.reset();
-
-                report.addMeasuredVariable(EXEC_TIME + memSize, timer.getElapsedTime());
-                timer.reset();
-            }
-
-            try {
-                report.generateDataFile(this.outDir);
-            }
-            catch (java.io.IOException e) {
-                e.printStackTrace();
-            }
-            //            System.out.println("\n---------------------> Test Completed <------------------ ");
-        }
+      for (int i = 0; i < this.count; i++) {
+        bootstrapMA.enter(bootstrapLogic);
+        report.addMeasuredVariable(CREATION_TIME + memSize, timer.getElapsedTime());
+        timer.reset();
+              
+              
+        report.addMeasuredVariable(ENTER_TIME + memSize, timerEnter.getElapsedTime());
+        timerEnter.reset();
+              
+        report.addMeasuredVariable(EXIT_TIME + memSize, timerExit.getElapsedTime());
+        timerExit.reset();
+              
+        report.addMeasuredVariable(EXEC_TIME + memSize, timerExec.getElapsedTime());
+        timerExec.reset();
+      }
+              
+            
+      try {
+        report.generateDataFile(this.outDir);
+      }
+      catch (java.io.IOException e) {
+        e.printStackTrace();
+      }
+      //            System.out.println("\n---------------------> Test Completed <------------------ ");
     }
+  }
 
-    static ArgParser parseArgs(String[] args) throws Exception {
+  static ArgParser parseArgs(String[] args) throws Exception {
         
-        CommandLineSpec cls = new CommandLineSpec();
-        cls.addRequiredArg(RTJPerfArgs.COUNT_OPT);
-        cls.addRequiredArg(RTJPerfArgs.SCOPED_MEMORY_TYPE_OPT);
-        cls.addRequiredArg(RTJPerfArgs.MEM_SIZE_OPT);
-        cls.addRequiredArg(RTJPerfArgs.OUT_DIR_OPT);
+    CommandLineSpec cls = new CommandLineSpec();
+    cls.addRequiredArg(RTJPerfArgs.COUNT_OPT);
+    cls.addRequiredArg(RTJPerfArgs.SCOPED_MEMORY_TYPE_OPT);
+    cls.addRequiredArg(RTJPerfArgs.MEM_SIZE_OPT);
+    cls.addRequiredArg(RTJPerfArgs.OUT_DIR_OPT);
         
-        ArgParser argParser = new ArgParser(cls, new ScopedMemoryTimingHelpHandler());
-        argParser.parse(args);
-        return argParser;
-    }
+    ArgParser argParser = new ArgParser(cls, new ScopedMemoryTimingHelpHandler());
+    argParser.parse(args);
+    return argParser;
+  }
     
-    public static void main(String[] args) throws Exception {
-        ArgParser argParser = parseArgs(args);
+  public static void main(String[] args) throws Exception {
+    ArgParser argParser = parseArgs(args);
 
-        Integer value = (Integer)argParser.getArg(RTJPerfArgs.COUNT_OPT).getValue();
-        final int count = value.intValue();
+    Integer value = (Integer)argParser.getArg(RTJPerfArgs.COUNT_OPT).getValue();
+    final int count = value.intValue();
 
-        value = (Integer)argParser.getArg(RTJPerfArgs.SCOPED_MEMORY_TYPE_OPT).getValue();
-        final int memType = value.intValue();
+    value = (Integer)argParser.getArg(RTJPerfArgs.SCOPED_MEMORY_TYPE_OPT).getValue();
+    final int memType = value.intValue();
 
-        value = (Integer)argParser.getArg(RTJPerfArgs.MEM_SIZE_OPT).getValue();
-        long memSize = value.intValue();
+    value = (Integer)argParser.getArg(RTJPerfArgs.MEM_SIZE_OPT).getValue();
+    long memSize = value.intValue();
 
-        String outDir = (String)argParser.getArg(RTJPerfArgs.OUT_DIR_OPT).getValue();
+    String outDir = (String)argParser.getArg(RTJPerfArgs.OUT_DIR_OPT).getValue();
 
-        TestLogic testLogic = new TestLogic(count,
-                          memSize,
-                          memType,
-                          outDir);
+    TestLogic testLogic = new TestLogic(count,
+                                        memSize,
+                                        memType,
+                                        outDir);
         
-        RealtimeThread rtThread =
-            new RealtimeThread(new PriorityParameters(30),
-                               null,
-                               null,
-                               ImmortalMemory.instance(),
-                               null,
-                               testLogic);
+    RealtimeThread rtThread =
+      new RealtimeThread(new PriorityParameters(30),
+                         null,
+                         null,
+                         ImmortalMemory.instance(),
+                         null,
+                         testLogic);
         
 
-        // -- Start the test --
-        rtThread.start();
+    // -- Start the test --
+    rtThread.start();
 
-        // Join the thread
-        rtThread.join();
-    }
+    // Join the thread
+    rtThread.join();
+  }
 }            
     
