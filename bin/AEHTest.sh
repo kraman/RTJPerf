9c4f511b192eda907647f104fdff819f418831a3
Angelo Corsaro <corsaro@doc.ece.uci.edu>
2002-12-13 05:56:28 +0000

    *** empty log message ***
    git-svn-id: file:///Users/kraman/Documents/personal/doc/RTJPerf/trunk@2623 bfff1265-3dd4-0310-90a8-ce2828190deb

---
+++ bin/AEHTest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export IMMORTAL_SIZE=6000000
+
+$RTJ_HOME/bin/tjvm -Xverify:none -Xms128M -Xbootclasspath:$RTJ_HOME/lib/foundation.jar -Djava.library.path=$JTOOLS_HOME/lib -Djava.class.path=$RTJP_HOME/classes:$JTOOLS_HOME/classes -Dproperties.load.path=$JTOOLS_HOME/bin edu.uci.ece.doc.rtjperf.asynch.DispatchDelayTestLauncher $*
+
